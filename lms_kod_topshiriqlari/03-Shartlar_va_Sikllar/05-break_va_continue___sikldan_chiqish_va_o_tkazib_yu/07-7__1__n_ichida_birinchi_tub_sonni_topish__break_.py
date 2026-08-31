b7eaef19ea4208644dda75d64f068859dd1a8508
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-08-31 21:48:21 +0500

    Add: 7) 1..n ichida birinchi tub sonni topish (break) [akbarazimqulov24-design]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/07-7__1__n_ichida_birinchi_tub_sonni_topish__break_.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n < 2:
+    print("No")
+else:
+    for i in range(2, n + 1):
+      # Tub son ekanligini tekshiramiz
+      tub = True
+      for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            tub = False
+            break
+      if tub:
+        print(i)
+        break
