60fe73c09c2e1ef3947cf575aa336c376fd69ead
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-19 00:05:57 +0500

    Add: Faqat juft ko'paytmalar [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Faqat_juft_ko_paytmalar.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Faqat_juft_ko_paytmalar.py
@@ -2,6 +2,6 @@ n = int(input())
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        product = i * j
-        if product % 2 == 0:
-            print(f"{i} x {j} = {product}")
+        prod = i * j
+        if prod % 2 == 0:
+            print(f"{i} x {j} = {prod}")
