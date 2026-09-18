0e27305ffc90756a735d0fa1d6489efb96af8e61
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-18 23:09:25 +0500

    Add: Eng katta va eng kichik farqi [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Eng_katta_va_eng_kichik_farqi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Eng_katta_va_eng_kichik_farqi.py
@@ -1,3 +1,14 @@
 n = int(input())
-m = [int(input()) for _ in range(n)]
-print(max(m) - min(m))
+
+first_num = int(input())
+mx = first_num
+mn = first_num
+
+for _ in range(n - 1):
+    num = int(input())
+    if num > mx:
+        mx = num
+    if num < mn:
+        mn = num
+        
+print(mx - mn)        
