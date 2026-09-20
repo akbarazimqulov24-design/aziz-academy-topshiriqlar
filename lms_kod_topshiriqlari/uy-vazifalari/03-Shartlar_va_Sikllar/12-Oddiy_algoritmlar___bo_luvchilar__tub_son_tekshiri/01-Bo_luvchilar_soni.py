706b5dd83c4172b7254df362ff89c9c4899d1ec7
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-20 22:03:24 +0500

    Add: Bo'luvchilar soni [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Bo_luvchilar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Bo_luvchilar_soni.py
@@ -1,11 +1,2 @@
 n = int(input())
-count = 0
-
-for i in range(1, int(n ** 0.5) + 1):
-    if n % i == 0:
-        if i * i == n:
-            count += 1
-        else:
-            count += 2
-            
-print(count)
+print(sum(1 for i in range(1, n + 1) if n % i == 0))
