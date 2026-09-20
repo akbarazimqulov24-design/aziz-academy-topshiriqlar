85020957acfa3fd032a5c4c78dc7326cc99bba75
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-20 22:07:22 +0500

    Add: Eng katta son [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Eng_katta_son.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Eng_katta_son.py
@@ -1,7 +1,3 @@
 n = int(input())
-m = int(input())
-for _ in range(n - 1):
-    x = int(input())
-    if x > m:
-        m = x
-print(m)
+numbers = [int(input()) for _ in range(n)]
+print(max(numbers))
