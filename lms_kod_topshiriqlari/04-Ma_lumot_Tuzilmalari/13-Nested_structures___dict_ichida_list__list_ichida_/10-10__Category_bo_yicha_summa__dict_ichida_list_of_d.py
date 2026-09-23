7c8704742036dc60869079f74b58fa1ed4569e2a
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-23 23:16:22 +0500

    Add: 10) Category bo‘yicha summa (dict ichida list of dicts) [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/10-10__Category_bo_yicha_summa__dict_ichida_list_of_d.py
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/10-10__Category_bo_yicha_summa__dict_ichida_list_of_d.py
@@ -1,12 +1,12 @@
 n = int(input().strip())
-items = []
+res = {}
+
 for _ in range(n):
     cat, name, price, qty = input().split()
-    items.append({'cat': cat, 'name': name, 'price': int(price), 'qty': int(qty)})
-
-totals = {}
-for x in items:
-    totals[x['cat']] = totals.get(x['cat'], 0) + x['price'] * x['qty']
+    res[cat] = res.get(cat, 0) + int(price) * int(qty)
     
-for cat in sorted(totals):
-    print(cat, totals[cat])
+for cat in sorted(res):
+    print(cat, res[cat])
+    
+
+
