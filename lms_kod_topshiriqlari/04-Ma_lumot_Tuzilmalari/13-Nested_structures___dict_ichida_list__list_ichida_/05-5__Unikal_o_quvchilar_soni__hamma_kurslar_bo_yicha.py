abe03f4fc2c19b429688d2ce1420968c3898da58
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-23 22:52:19 +0500

    Add: 5) Unikal o‘quvchilar soni (hamma kurslar bo‘yicha) [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/05-5__Unikal_o_quvchilar_soni__hamma_kurslar_bo_yicha.py
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/05-5__Unikal_o_quvchilar_soni__hamma_kurslar_bo_yicha.py
@@ -1,1 +1,13 @@
-adfbdfbdf
+import sys
+
+d = sys.stdin.read().split()
+s = set()
+i = 1
+
+for _ in range(int(d[0])):
+    k = int(d[i+1])
+    # d[i+2] dan boshlanib qirqib olamiz (d[i+1] dagi k soni tushib qoladi)
+    s.update(d[i + 2 : i + 2 + k])
+    i += 2 + k
+    
+print(len(s))
