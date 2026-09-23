39d9349bcf57ca85c9407b1d4c5c27957256a56b
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-23 23:40:15 +0500

    Add: 15) Eng ko‘p tagli user (nested max) [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/15-15__Eng_ko_p_tagli_user__nested_max_.py
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/15-15__Eng_ko_p_tagli_user__nested_max_.py
@@ -1,11 +1,3 @@
-n = int(input().strip())
-users = []
-for _ in range(n):
-    parts = input().split()
-    username = parts[0]
-    k = int(parts[1])
-    tags = parts[2:2+k]
-    users.append({'username': username, 'tags': tags})
-
-best_user = max(users, key=lambda u: len(u['tags']))
-print(best_user['username'])
+n = int(input())
+users = [input().split() for _ in range(n)]
+print(max(users, key= lambda x: int(x[1]))[0])
