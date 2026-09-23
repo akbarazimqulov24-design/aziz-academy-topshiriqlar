db42c672e0b8f7405a351914a87b57bde454c03f
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-23 09:11:45 +0500

    Add: 7) Mahsulotlar: umumiy summa (list ichida dict) [akbarazimqulov24-design]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/13-Nested_structures___dict_ichida_list__list_ichida_/07-7__Mahsulotlar__umumiy_summa__list_ichida_dict_.py
@@ -0,0 +1,7 @@
+n = int(input().strip())
+items = []
+for _ in range(n):
+    name, price, qty = input().split()
+    items.append({'name': name, 'price': int(price), 'qty': int(qty)})
+
+print(sum(item['price'] * item['qty'] for item in items))
