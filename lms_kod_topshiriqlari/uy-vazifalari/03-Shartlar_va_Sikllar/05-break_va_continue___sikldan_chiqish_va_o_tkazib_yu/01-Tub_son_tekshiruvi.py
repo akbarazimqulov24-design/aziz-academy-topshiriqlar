1db62cc02f5bb9bf7383bcc11f20c4b13b149238
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-24 21:32:16 +0500

    Add: Tub son tekshiruvi [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_son_tekshiruvi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_son_tekshiruvi.py
@@ -1,15 +1,12 @@
 n = int(input())
 
-if n <= 1:
-  print("yo'q")
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print("yo'q")
+            break
+    else:
+        print("ha")
 else:
-  tub = True
-  for i in range(2, int(n**0.5) + 1):
-    if n % i == 0:
-      tub = False
-      break
-        
-  if tub:
-    print("ha")
-  else:
-    print("yo'q")
+        print("yo'q")
+            
