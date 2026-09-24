c1d6837e0259bed1ee5771e3877af75f85fc029d
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-24 20:04:09 +0500

    Add: t ga bo'linadigan katakchalar soni [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-t_ga_bo_linadigan_katakchalar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-t_ga_bo_linadigan_katakchalar_soni.py
@@ -1,2 +1,5 @@
-n, t = int(input()), int(input())
-print(sum(1 for i in range(1, n + 1) for j in range(1, n + 1) if (i * j) % t == 0))
+n = int(input())
+t = int(input())
+
+count = sum(1 for i in range(1, n + 1) for j in range(1, n + 1) if (i * j) % t == 0)
+print(count)
