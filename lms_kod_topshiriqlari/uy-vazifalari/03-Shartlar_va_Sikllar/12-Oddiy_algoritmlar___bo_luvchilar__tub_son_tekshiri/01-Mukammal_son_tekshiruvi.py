3e2e1947c82ba171ce2ea9455733160c44e31871
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-26 21:21:27 +0500

    Add: Mukammal son tekshiruvi [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Mukammal_son_tekshiruvi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-Mukammal_son_tekshiruvi.py
@@ -1,6 +1,11 @@
 n = int(input())
-print(
-    "MUKAMMAL"
-    if sum(i for i in range(1, n) if n % i == 0) == n
-    else "MUKAMMAL EMAS"
-)
+
+yigindi = 0
+for i in range(1, n):
+    if n % i == 0:
+        yigindi += i
+        
+if yigindi == n:
+    print("MUKAMMAL")
+else:
+    print("MUKAMMAL EMAS")
