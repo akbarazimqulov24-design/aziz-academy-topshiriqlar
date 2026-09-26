09a196ad3453700863aaa4b9d1537ac6df43136f
akbarazimqulov24-design <akbarazimqulov24@gmail.com>
2026-09-26 21:26:17 +0500

    Add: Tub sonlar sanog'i [akbarazimqulov24-design]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_sonlar_sanog_i.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_sonlar_sanog_i.py
@@ -1,12 +1,14 @@
-c = 0
-while (n := int(input())) != 0:
+import sys
+
+sanoq = 0
+for x in sys.stdin.read().split():
+    n = int(x)
+    if n == 0: break
     if n < 2: continue
-    t = True
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            t = False
-            break
-        i += 1
-    if t: c += 1
-print(c)
+    
+    d = 2
+    while d * d <= n and n % d != 0: d += 1
+    if d * d > n: sanoq += 1
+    
+print(sanoq)
+    
